0018f3bb923f65480a5e93eb6f524e6122cb5ac8
Gian Luca Brunetti <gianluca.brunetti@gmail.com>
2014-04-23 00:48:56 +0000

    Import of GLBRUNE/Sim-OPTS-0.24 from CPAN.
    gitpan-cpan-distribution: Sim-OPTS
    gitpan-cpan-version:      0.24
    gitpan-cpan-path:         GLBRUNE/Sim-OPTS-0.24.tar.gz
    gitpan-cpan-author:       GLBRUNE
    gitpan-cpan-maturity:     released

--- lib/Sim/OPTS.pm
+++ lib/Sim/OPTS.pm
@@ -25,7 +25,7 @@ use warnings; # use warnings;
 %EXPORT_TAGS = ( DEFAULT => [qw(&opts &prepare)]); # our %EXPORT_TAGS = ( 'all' => [ qw( ) ] );
 @EXPORT_OK   = qw(); # our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 @EXPORT = qw(); # our @EXPORT = qw( );
-$VERSION = '0.23_1'; # our $VERSION = '';
+$VERSION = '0.24'; # our $VERSION = '';
 $ABSTRACT = 'OPTS is a program conceived to manage parametric explorations through the use of the ESP-r building performance simulation platform.';
 
 
@@ -78,9 +78,6 @@ $Data::Dumper::Indent = 0;
 $Data::Dumper::Useqq  = 1;
 $Data::Dumper::Terse  = 1;
 
-
-###########################################################################################
-###########################################################################################
 ###########################################################################################
 # BELOW THE OPTS PROGRAM FOLLOWS.
 my $varprov;
